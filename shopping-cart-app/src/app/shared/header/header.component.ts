c3c5c818fe6a8eea0c4f07d954f3e1b683144dd9
Peter Bartolo <pbartolo@crimsowning.com>
2017-04-25 17:30:13 +0200

    made the header display the logged in user

--- shopping-cart-app/src/app/shared/header/header.component.ts
+++ shopping-cart-app/src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../authentication/service/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  signOut() {
+    this.authService.logout();
   }
 }
